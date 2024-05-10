d120770884c3a5b0ac511dd0992222e3252cc2ae
Ashikam263 <ashikam263@gmail.com>
2024-05-10 10:16:22 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -115,13 +115,13 @@ class App extends Component {
             <div className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link to={"/login"} className="nav-link">
-                  Login
+                  LogIn
                 </Link>
               </li>
 
               <li className="nav-item">
                 <Link to={"/register"} className="nav-link">
-                  Sign Up
+                  SignUp
                 </Link>
               </li>
             </div>
